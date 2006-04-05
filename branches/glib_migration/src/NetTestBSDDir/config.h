33817df8b9bd428aa792dec42b1445c29c79d776
raj <raj@44b83d64-6b03-0410-99c4-fe3b3a721505>
2006-04-05 01:00:01 +0000

    Checkin some changes related to building netperf under Windows
    git-svn-id: http://www.netperf.org/svn/netperf4@126 44b83d64-6b03-0410-99c4-fe3b3a721505

---
+++ branches/glib_migration/src/NetTestBSDDir/config.h
@@ -0,0 +1,16 @@
+
+#define WITH_GLIB 1
+#define HAVE_FCNTL_H 1
+#define HAVE_LIMITS_H 1
+#define HAVE_MATH_H 1
+#define HAVE_STDLIB_H 1
+#define HAVE_STDIO_H 1
+#define HAVE_STRING_H 1
+#define HAVE_WINDOWS_H 1
+#define HAVE_WINSOCK2_H 1
+#define HAVE_WS2TCPIP_H 1
+
+
+#define HAVE_GETADDRINFO 1
+#define HAVE_GETNAMEINFO 1
+
